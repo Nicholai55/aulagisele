1cae60c362c7d857090bc4f139f019e4ad2e18dd
Nicholai55 <nicholai.gomes@faculdadecesusc.edu.br>
2020-09-09 21:35:01 -0300

    Update exercicio18.py

--- exercicio18.py
+++ exercicio18.py
@@ -1,11 +1,47 @@
-class animal():
+class Animal():
 
-    def ataque(self):
-        print("INICIANDO O ATAQUE")
+    def __init__(self):
+        #print("INICIANDO O ATAQUE!!!")
+        pass
 
     def comer(self):
         print("o animal esta comendo")
 
-cachorro = animal()
-cachorro.ataque
-cachorro.comer
+
+class Cachorro(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def comer(self):
+        print("o animal esta comendo ração")
+
+
+class Cavalo(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def comer(self):
+        print("o animal esta comendo feno")
+
+
+class Gato(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def comer(self):
+        print("o animal esta comendo atum")
+
+
+class AnimalTeste():
+    def executando(self):
+        cachorro = Cachorro()
+        cavalo = Cavalo()
+        gato = Gato()
+        lista_animais = [cachorro, cavalo, gato]
+
+        for i in lista_animais:
+            i.comer()
+
+
+teste_animal = AnimalTeste()
+teste_animal.executando()
