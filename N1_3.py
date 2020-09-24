37096625f24c796e524e9b67d8bdf5cca7bd18be
Vini0410 <vini0420@gmail.com>
2020-09-24 20:51:37 -0300

    Envio N1
    envio das questões 3 e 4 da n1

---
+++ N1_3.py
@@ -0,0 +1,49 @@
+while True:
+    pergunta = int(input("Digite uma nota de '0' a '10': "))
+
+    if pergunta == 0:
+        print("Nota Zero cadastrada")
+        break
+
+    elif pergunta == 1:
+        print("nota Um cadastrada")
+        break
+
+    elif pergunta == 2:
+        print("nota Dois cadastrada")
+        break
+
+    elif pergunta == 3:
+        print("nota Tres cadastrada")
+        break
+
+    elif pergunta == 4:
+        print("nota Quatro cadastrada")
+        break
+
+    elif pergunta == 5:
+        print("nota Cinco cadastrada")
+        break
+
+    elif pergunta == 6:
+        print("nota Seis cadastrada")
+        break
+
+    elif pergunta == 7:
+        print("nota Sete cadastrada")
+        break
+
+    elif pergunta == 8:
+        print("nota Oito cadastrada")
+        break
+
+    elif pergunta == 9:
+        print("nota Nove cadastrada")
+        break
+
+    elif pergunta == 10:
+        print("nota Dez cadastrada")
+        break
+
+    else:
+        print("Digite um numero valido")
